1d40ec6f5116b9ecb0e1051cf77c4134a9590489
Jamie Rumbelow <jamie@jamierumbelow.net>
2012-08-08 15:57:15 +0300

    Finally adding the rest of the code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CodeIgniter Handbook - Vol 1 - Who Needs Ruby?
-## https://efendibooks.com/books/codeigniter-handbook-vol-1
+### https://efendibooks.com/books/codeigniter-handbook-vol-1
 
 This repository is the complete code for the hugely popular CodeIgniter Handbook, Volume 1, by [Jamie Rumbelow](http://jamierumbelow.net). It's not a fully functional application; it only contains the code that is created in the book.
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,95 @@
+<?php
+
+class MY_Controller extends CI_Controller
+{
+	public $models = array();
+	public $data = array();
+
+	public $view = TRUE;
+	public $layout = TRUE;
+
+	public $before_filters = array();
+	public $after_filters = array();
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('inflector');
+		
+		$model = strotolower(singular(get_class($this)));
+
+		if (file_exists(APPPATH . 'models/' . $model . '_model.php'))
+		{
+			$this->models[] = $model;
+		}
+
+		foreach ($this->models as $model)
+		{
+			$this->load->model($model . '_model', $model);
+		}
+	}
+
+	public function _remap($method, $parameters)
+	{
+		if (method_exists($this, $method))
+		{
+			$this->_run_before_filters($method, $parameters);
+				call_user_func_array(array($this, $method), $parameters);
+			$this->_run_after_filters($method, $parameters);
+		}
+		else
+    	{
+        	show_404();
+		}
+
+		$view = (is_string($this->view) && !empty($this->view)) ? $this->view : $view;
+
+		if ($this->view !== FALSE)
+		{
+			$this->data['yield'] = $this->load->view($view, $this->data, TRUE);
+
+			if (is_string($this->layout) && !empty($this->layout))
+			{
+				$layout = $this->layout;
+			}
+			elseif (file_exists(APPPATH . 'views/layouts/' . strtolower(get_class($this)) . '.php'))
+			{
+				$layout = 'layouts/' . strtolower(get_class($this));
+			}
+			else
+			{
+				$layout = 'layouts/application';
+			}
+
+			if ($this->layout)
+			{
+				$this->load->view($layout, $this->data);
+			}
+			else
+			{
+				echo $this->data['yield'];
+			}
+		}
+	}
+
+	protected function _run_filters($what, $action, $parameters)
+	{
+		$what = $what . '_filters';
+		
+		foreach ($this->$what as $filter => $details)
+		{
+			if (is_string($details))
+			{
+				$this->$details($action, $parameters);
+			}
+			elseif (is_array($details))
+			{
+				if (in_array($action, @$details['only']) || !in_array($action, @$details['except']))
+				{
+					$this->$filter($action, $parameters);
+				}
+			}
+		}
+	}
+}
